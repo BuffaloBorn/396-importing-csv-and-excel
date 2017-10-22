49f7a4d90650e947a5882ef411b0689bb0c6a335
Carl McGruder <carl.mcgruder@gmail.com>
2017-10-22 18:31:42 -0400

    Fixed the CSV testing
    Updated test/products.csv for testing

--- store/app/models/product_import.rb
+++ store/app/models/product_import.rb
@@ -45,9 +45,9 @@ class ProductImport
 
   def open_spreadsheet
     case File.extname(file.original_filename)
-    when ".csv" then Csv.new(file.path, nil, :ignore)
-    when ".xls" then Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+    when ".csv" then Roo::CSV.new(file.path)
+    when ".xls" then Roo::Excel.new(file.path)
+    when ".xlsx" then Roo::Excelx.new(file.path)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
